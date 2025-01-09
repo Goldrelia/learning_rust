28a3386c0cee57c0919a823591f1bf66300ae035
Goldrelia <aurelia.bouliane@gmail.com>
2025-01-09 18:04:45 -0500

    finished first exercise

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,44 @@
+const STARTING_MISSILES: i32 = 8;
+
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let mut missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
+}
+
+// Extra challenge 1
+
+// fn main() {
+    // let mut missiles: i32 = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+
+// }
+
+// Extra challenge 2
+
+// fn main() {
+    // let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    // println!("Firing {} of my {} missiles...", ready, missiles);
+
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+// }
+
+// Extra challenge 3 
+
+// fn main() {
+//     let (mut missiles, ready)(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+//     println!("Firing {} of my {} missiles...", ready, missiles);
+
+//     missiles = missiles - ready;
+//     println!("{} missiles left", missiles);
+// }
